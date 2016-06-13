0cebde389678db239ee79361e7394381210539db
adwalvekar <aditya.s.walvekar@gmail.com>
2016-06-13 10:40:08 -0400

    First Commit

---
+++ __init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://aditya:Bangalore12E@localhost/flask'
+class User(db.Model):
+	uid = db.Column(db.Integer, primary_key= True)
+	username = db.Column(db.String(20), unique=True)
+	password = db.Column(db.String(20), unique= True)
+
+	def __init__(self,username, password):
+		self.username = username
+		self.password = password
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/full')
+def full():
+	return render_template('main.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+if __name__ == "__main__":
+	app.run()
